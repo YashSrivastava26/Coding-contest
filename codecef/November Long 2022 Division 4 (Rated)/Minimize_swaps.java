de1c555fb2ec0a76cad849b8864d391a77f2bb88
YashSrivastava26 <yashsrivastava2603@gmail.com>
2022-11-15 10:07:20 +0530

    15 nov

--- codecef/November Long 2022 Division 4 (Rated)/Minimize_swaps.java
+++ codecef/November Long 2022 Division 4 (Rated)/Minimize_swaps.java
@@ -2,10 +2,111 @@ import java.util.Scanner;
 
 class Minimize_swaps
 {
-    static boolean checkDivBy3(int evenOnes, int oddOnes){
-        if((evenOnes - oddOnes) % 3 == 0 || (oddOnes - evenOnes) % 3 == 0) return true;
-        else return false;
+    static void solution(String st){
+        int size = st.length();
+        int evenPlaceOnes = 0, oddPlaceOnes = 0;
+
+        for (int i = 0; i < size; i++) {
+            if(st.charAt(i) == '1'){
+                if((i & 1) == 0) evenPlaceOnes++;
+                else oddPlaceOnes++;
+            }
+        }
+        if(Math.abs(oddPlaceOnes - evenPlaceOnes) % 3 == 0){
+            System.out.println("0");
+            return;
+        }
+        else{
+            int freq = 0;
+            for (int i = 1; i < size; i++) {
+                if(st.charAt(i) != st.charAt(i - 1))
+                    freq++;
+            }
+
+            if(freq == 1){
+                for (int i = 1; i < size; i++) {
+                    if( ((i % 2) == 1 || ((size - i) % 2) == 1) && st.charAt(i) != st.charAt(i - 1) && i != 1 && i != size - 1){
+                        System.out.println("3");
+                        return;
+                    }
+                }
+            }
+
+            if(Math.abs(oddPlaceOnes - evenPlaceOnes) % 3 == 2){
+                if(oddPlaceOnes + evenPlaceOnes == size) System.out.println("-1");
+                else System.out.println("1");
+            }
+            else{
+                if(oddPlaceOnes + evenPlaceOnes > size - 1)
+                    System.out.println("-1");
+                else{
+                    boolean flag = false;
+                    
+                    for (int i = 0; i < size; i++) {
+                        if(evenPlaceOnes > oddPlaceOnes){
+                            if(i % 2 != 0 && st.charAt(i) == '1'){
+                                if(i - 1 >= 0 && st.charAt(i - 1) == '0'){
+                                    flag = true;
+                                    break;
+                                }
+                                else if(i + 1 < size && st.charAt(i + 1) == '0'){
+                                    flag = true;
+                                    break;
+                                }
+                            }
+
+                        }
+                        else{
+                            if(i % 2 == 0 && st.charAt(i) == '1'){
+                                if(i - 1 >= 0 && st.charAt(i - 1) == '0'){
+                                    flag = true;
+                                    break;
+                                }
+                                else if(i + 1 < size && st.charAt(i + 1) == '0'){
+                                    flag = true;
+                                    break;
+                                }
+                            } 
+                        }
+                    }
+
+
+                    if(flag) System.out.println("1");
+                    
+                    else{
+                        if(oddPlaceOnes + evenPlaceOnes == size - 1){
+                            System.out.println("-1");
+                        }
+                        else{
+                            if(oddPlaceOnes > evenPlaceOnes){
+                                int cnt = 0;
+                                for (int i = 0; i < size; i++) {
+                                    if(i % 2 == 0) cnt++;
+                                }
+
+                                if(oddPlaceOnes < 2 || cnt < 2) System.out.println("-1");
+                                else System.out.println("2");
+
+                            }
+                            else{
+                                int cnt = 0;
+                                for (int i = 0; i < size; i++) {
+                                    if(i % 2 != 0) cnt++;
+                                }
+
+                                if(evenPlaceOnes < 2 || cnt < 2) System.out.println("-1");
+                                else System.out.println("2");
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
     }
+
+
 	public static void main (String[] args) 
 	{
         
@@ -13,43 +114,7 @@ class Minimize_swaps
         int t = s.nextInt();
         while(t-- != 0){
             String st = s.next();
-            int evenPlaceOnes = 0;
-            int oddPlaceOnes = 0;
-            int evenPlaceZeros = 0;
-            int oddPlaceZeros = 0;
-            for (int i = 0; i < st.length(); i++) {
-                if(st.charAt(i) == '1'){
-                    if(i % 2 == 0) evenPlaceOnes++;
-                    else oddPlaceOnes++;
-                }
-                if(st.charAt(i) == '0'){
-                    if(i % 2 == 0) evenPlaceZeros++;
-                    else oddPlaceZeros++;
-                }
-            }
-            if(checkDivBy3(evenPlaceOnes, oddPlaceOnes)) System.out.println("0");
-
-            else if(evenPlaceZeros >= 1 && oddPlaceOnes >= 1 && checkDivBy3(evenPlaceOnes + 1, oddPlaceOnes - 1)) System.out.println("1");
-            else if(oddPlaceZeros >= 1 && evenPlaceOnes >= 1 && checkDivBy3(evenPlaceOnes - 1, oddPlaceOnes + 1)) System.out.println("1");
-
-            else if(evenPlaceZeros >= 2 && oddPlaceOnes >= 2 && checkDivBy3(evenPlaceOnes + 2, oddPlaceOnes - 2)) System.out.println("2");
-            else if(oddPlaceZeros >= 2 && evenPlaceOnes >= 2 && checkDivBy3(evenPlaceOnes - 2, oddPlaceOnes + 2)) System.out.println("2");
-            
-            else if(evenPlaceZeros >= 3 && oddPlaceOnes >= 3 && checkDivBy3(evenPlaceOnes + 3, oddPlaceOnes - 3)) System.out.println("3");
-            else if(oddPlaceZeros >= 3 && evenPlaceOnes >= 3 && checkDivBy3(evenPlaceOnes - 3, oddPlaceOnes + 3)) System.out.println("3");
-
-            else if(evenPlaceZeros >= 4 && oddPlaceOnes >= 4 && checkDivBy3(evenPlaceOnes + 4, oddPlaceOnes - 4)) System.out.println("4");
-            else if(oddPlaceZeros >= 4 && evenPlaceOnes >= 4 && checkDivBy3(evenPlaceOnes - 4, oddPlaceOnes + 4)) System.out.println("4");
-
-            else if(evenPlaceZeros >= 5 && oddPlaceOnes >= 5 && checkDivBy3(evenPlaceOnes + 5, oddPlaceOnes - 5)) System.out.println("5");
-            else if(oddPlaceZeros >= 5 && evenPlaceOnes >= 5 && checkDivBy3(evenPlaceOnes - 5, oddPlaceOnes + 5)) System.out.println("5");
-
-            else if(evenPlaceZeros >= 6 && oddPlaceOnes >= 6 && checkDivBy3(evenPlaceOnes + 6, oddPlaceOnes - 6)) System.out.println("6");
-            else if(oddPlaceZeros >= 6 && evenPlaceOnes >= 6 && checkDivBy3(evenPlaceOnes - 6, oddPlaceOnes + 6)) System.out.println("6");
-
-            else System.out.println("-1");
-            
-            
+            solution(st);
         }
 	}
 }
